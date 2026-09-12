0f38bdee281ab47f00fbb4efeaaf1994cbea3f25
Jack Tyler <jacktyler@unifiedstack.io>
2026-09-12 08:30:47 -0700

    Third correction on successful form submission.

--- api/contact.js
+++ api/contact.js
@@ -334,7 +334,7 @@ export default async function handler(req, res) {
       status: 'new'
     });
 
-    return respondSuccess(res);
+    return respondSuccess(req, res);
   } catch (error) {
     if (error.message === 'BODY_TOO_LARGE') {
       return res.status(413).json({
